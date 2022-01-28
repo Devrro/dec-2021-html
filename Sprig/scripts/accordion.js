c543e10dca0bae436359526da041e40ed5efb9f1
Serhii_Terletckiy <beinght@gmail.com>
2022-01-28 20:29:37 +0200

    --- What changed? --- 1.Fully finished Sprig for 1920 res

---
+++ Sprig/scripts/accordion.js
@@ -0,0 +1,33 @@
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+//
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     }
+//   });
+// }
+var acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    for (let j = 0; j < acc.length; j++) {
+      acc[j].classList.remove("active");
+      if(j!=i){
+        acc[j].nextElementSibling.style.maxHeight = null;
+      }
+    }
+    this.classList.add("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
